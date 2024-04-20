1ef623f46598b1c6cde6176b9a109de86ae2e331
Evgeniy K <kovalev8915512@gmail.com>
2024-04-20 13:38:08 +0300

    Добавлена программа

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿
+      
+    int count=0;
+    
+    string [] arr1= new string [] {"Sunday", "Mon", "Tuesday", "Wed", "Thu", "Friday", "Sat"};
+
+for (int i=0; i< arr1.Length; i++){
+    if (arr1[i].Length<4){
+        count++;
+    }
+    
+
+
+}
+string [] arr2=new string [count];
+
+count=0;
+for (int i=0; i< arr1.Length; i++){
+    if (arr1[i].Length<4){
+        arr2[count]=arr1[i];
+        count++;
+    }
+}
+
+Console.WriteLine($"Начальный массив: [{string.Join(", ", arr1)}]");
+Console.WriteLine($"Конечный массив: [{string.Join(", ", arr2)}]");
+
+
+
+
